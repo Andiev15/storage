6888c5013e643289cc00c919ffb81c79aeaa567a
Andiev <oandiev@ozon.ru>
2021-12-01 14:42:55 +0300

    initial commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+import "github.com/Andiev15/storage/internal/storage"
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt",[]byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Work", file)
+}
